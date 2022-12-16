e1d31026f6af8b143792d10898a53a8f0f14617a
Robert Printz <robert.printz@daveramsey.com>
2022-12-15 18:28:37 -0600

    Day16 prep

---
+++ src/main/java/day16/Day16.java
@@ -0,0 +1,24 @@
+package day16;
+
+import common.AdventOfCodeBase;
+import common.Part;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+
+
+public class Day16 extends AdventOfCodeBase {
+
+    public Day16(String inputFilename, Part part) {
+        super(inputFilename, part);
+    }
+
+    @Override
+    public String run() {
+        int answer = 0;
+
+        return "" + answer;
+    }
+
+}
+
---
+++ src/test/java/day16/Day16Test.java
@@ -0,0 +1,46 @@
+package day16;
+
+import common.Part;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day16Test {
+
+    private static final String INPUT = "/input/day16_input.txt";
+    private static final String SAMPLE_INPUT = "/input/day16_sample_input.txt";
+
+
+    @Test
+    void run_part1_sample() {
+        Day16 day = new Day16(SAMPLE_INPUT, Part.PART_ONE);
+        String result = day.run();
+        System.out.println("result= " + result);
+        assertEquals(0, Integer.valueOf(result));
+    }
+
+    @Test
+    void run_part1() {
+        Day16 day = new Day16(INPUT, Part.PART_ONE);
+        String result = day.run();
+        System.out.println("result= " + result);
+        assertEquals(0, Integer.valueOf(result));
+    }
+
+    @Test
+    void run_part2_sample() {
+        Day16 day = new Day16(SAMPLE_INPUT, Part.PART_TWO);
+        String result = day.run();
+        System.out.println("result= " + result);
+        assertEquals(0, Integer.valueOf(result));
+    }
+
+    @Test
+    void run_part2() {
+        Day16 day = new Day16(INPUT, Part.PART_TWO);
+        String result = day.run();
+        System.out.println("result= " + result);
+        assertEquals(0, Integer.valueOf(result));
+    }
+
+}
